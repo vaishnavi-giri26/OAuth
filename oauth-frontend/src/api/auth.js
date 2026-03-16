2c8c6a37a413d3df8acd86d6d12568c3bf49571d
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-16 20:48:21 +0530

    Update auth.js

--- oauth-frontend/src/api/auth.js
+++ oauth-frontend/src/api/auth.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
-  //baseURL = "https://oauth-backend-8a74.onrender.com/api",
+  baseURL: "https://oauth-backend-s5bk.onrender.com",
   withCredentials: true
 });
