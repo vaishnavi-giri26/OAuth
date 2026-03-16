1bcc8b64c19fe525525b427a874d57895edec9ed
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-16 20:58:07 +0530

    Update App.jsx

--- oauth-frontend/src/App.jsx
+++ oauth-frontend/src/App.jsx
@@ -16,8 +16,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   // Use the Netlify variable, or fallback to localhost for development
-  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-//const API_BASE_URL = "https://oauth-backend-8a74.onrender.com";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "https://oauth-backend-s5bk.onrender.com";
   useEffect(() => {
     const fetchUser = async () => {
       try {
