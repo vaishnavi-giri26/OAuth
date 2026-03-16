35170f4c6b81ebd99a10ecd332a1dfdecc279555
GIRI VAISHNAVI <vaishnavigiri2612@gmail.com>
2026-03-16 21:06:20 +0530

    Update server.js

--- oauth-backend/server.js
+++ oauth-backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 ================================ */
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://oauth-frontend-esm9.onrender.com",
     credentials: true,
   })
 );
